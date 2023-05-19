1ddad659709cc7878e8a1cc5386c4df7a2c88745
Erdenedalai Erdenebat <erdenedalai.er@novelsoft.mn>
2023-05-19 13:43:36 +0800

    ads

--- main.cpp
+++ main.cpp
@@ -1,50 +1,49 @@
 #include <GL/glut.h>
 #include <math.h>
-#include<stdio.h>
+#include <stdio.h>
+#include <fstream>
+#include <sstream>
+#include <vector>
 
-float angle = 0.0; // Used for movement of legs and arms
-float camX = 0, camZ = 0;
+float angle = 0.0;
+float camX = 0, camY = 0, camZ = 0;
+float realX = 0, realZ = -7.5;
+float jumpSpeed = 0;
 
 void drawRoom()
 {
     glBegin(GL_QUADS);
 
-    // Floor
     glColor3f(0.0f, 1.0f, 0.0f);
     glVertex3f(-10.0f, -1.0f, -10.0f);
     glVertex3f(10.0f, -1.0f, -10.0f);
     glVertex3f(10.0f, -1.0f, 10.0f);
     glVertex3f(-10.0f, -1.0f, 10.0f);
 
-    // Ceiling
     glColor3f(1.0f, 1.0f, 0.0f);
     glVertex3f(-10.0f, 3.0f, -10.0f);
     glVertex3f(10.0f, 3.0f, -10.0f);
     glVertex3f(10.0f, 3.0f, 10.0f);
     glVertex3f(-10.0f, 3.0f, 10.0f);
 
-    // Front wall
     glColor3f(1.0f, 0.0f, 0.0f);
     glVertex3f(-10.0f, -1.0f, -10.0f);
     glVertex3f(10.0f, -1.0f, -10.0f);
     glVertex3f(10.0f, 3.0f, -10.0f);
     glVertex3f(-10.0f, 3.0f, -10.0f);
 
-    // Back wall
     glColor3f(0.0f, 0.0f, 1.0f);
     glVertex3f(-10.0f, -1.0f, 10.0f);
     glVertex3f(10.0f, -1.0f, 10.0f);
     glVertex3f(10.0f, 3.0f, 10.0f);
     glVertex3f(-10.0f, 3.0f, 10.0f);
 
-    // Left wall
     glColor3f(0.0f, 1.0f, 1.0f);
     glVertex3f(-10.0f, -1.0f, -10.0f);
     glVertex3f(-10.0f, -1.0f, 10.0f);
     glVertex3f(-10.0f, 3.0f, 10.0f);
     glVertex3f(-10.0f, 3.0f, -10.0f);
 
-    // Right wall
     glColor3f(1.0f, 0.0f, 1.0f);
     glVertex3f(10.0f, -1.0f, -10.0f);
     glVertex3f(10.0f, -1.0f, 10.0f);
@@ -56,27 +55,32 @@ void drawRoom()
 
 void drawRobot()
 {
-    
-
-    // Draw head (a simple cube)
-    
-    glTranslatef(camX, -0.5f, camZ-3.0f);
-    glColor3f(1.0, 1.0, 1.0); // White
-    glTranslatef(0.0, 0.6, 0.0);
+    glTranslatef(camX, camY - 0.5, camZ);
+    glColor3f(1.0, 1.0, 1.0);
+    glTranslatef(0.0, 1.2, 0.0);
     glutSolidCube(0.4);
 
-    // Draw body (a simple cube)
-    glColor3f(0.0, 0.0, 1.0); // Red
+    glColor3f(0.5, 0.5, 0.5);
+    glTranslatef(0.0, -0.3, 0.0);
+    glutSolidCube(0.2);
+
+    glColor3f(0.5, 0.5, 0.5);
+    GLUquadricObj *cylinder = gluNewQuadric();
+    glTranslatef(0.0, -0.6, 0.0);
+    glRotatef(-90.0, 1.0, 0.0, 0.0);
+    gluCylinder(cylinder, 0.35, 0.35, 0.6, 10, 10);
+    glRotatef(90.0, 1.0, 0.0, 0.0);
+    gluDeleteQuadric(cylinder);
+
+    glColor3f(0.5, 0.5, 0.5);
     glutSolidCube(0.7);
 
-    // Draw head (a simple cube)
     glPushMatrix();
-    glColor3f(1.0, 1.0, 1.0); // White
+    glColor3f(1.0, 1.0, 1.0);
     glTranslatef(0.0, 0.6, 0.0);
     glutSolidCube(0.4);
 
-    // Draw eyes on the head (two small black spheres)
-    glColor3f(0.0, 0.0, 0.0); // Black
+    glColor3f(0.0, 0.0, 0.0);
     glPushMatrix();
     glTranslatef(-0.1, 0.1, 0.2);
     glutSolidSphere(0.04, 10, 10);
@@ -89,75 +93,83 @@ void drawRobot()
 
     glPopMatrix();
 
-    // Draw legs (two simple cubes)
     glPushMatrix();
-    glColor3f(0.0, 0.0, 1.0); // Blue
+    glColor3f(0.5, 0.5, 0.5);
     glTranslatef(-0.2, -0.6, 0.0);
     glRotatef(sin(angle) * 45, 1, 0, 0);
     glutSolidCube(0.2);
     glPopMatrix();
 
     glPushMatrix();
-    glColor3f(0.0, 0.0, 1.0); // Blue
+    glColor3f(0.5, 0.5, 0.5);
     glTranslatef(0.2, -0.6, 0.0);
     glRotatef(sin(angle + M_PI) * 45, 1, 0, 0);
     glutSolidCube(0.2);
     glPopMatrix();
 
-    // Draw arms with hands (simple cubes with smaller cubes as hands)
     glPushMatrix();
-    glColor3f(0.0, 0.0, 1.0); // Blue
+    glColor3f(0.5, 0.5, 0.5);
     glTranslatef(-0.45, 0.0, 0.0);
     glRotatef(sin(angle) * 45, 1, 0, 0);
     glutSolidCube(0.2);
 
-    // Draw the hand
-    glColor3f(1.0, 1.0, 1.0); // White
+    glColor3f(1.0, 1.0, 1.0);
     glTranslatef(0.0, -0.15, 0.0);
     glutSolidCube(0.1);
 
     glPopMatrix();
 
     glPushMatrix();
-    glColor3f(0.0, 0.0, 1.0); // Blue
+    glColor3f(0.5, 0.5, 0.5);
     glTranslatef(0.45, 0.0, 0.0);
     glRotatef(sin(angle + M_PI) * 45, 1, 0, 0);
     glutSolidCube(0.2);
 
-    // Draw the hand
-    glColor3f(1.0, 1.0, 1.0); // White
+    glColor3f(1.0, 1.0, 1.0);
     glTranslatef(0.0, -0.15, 0.0);
     glutSolidCube(0.1);
 
     glPopMatrix();
-    angle += 0.05; // Increase angle to provide the illusion of movement
+    angle += 0.05;
 
-     glLoadIdentity();
-    glTranslatef(camX, -0.5f, camZ-3.0f);
+    glLoadIdentity();
+    glTranslatef(camX, -0.5f, camZ - 3.0f);
 
     glLoadIdentity();
     glTranslatef(0.0f, -0.5f, -3.0f);
 }
 
-void display(void) {
+void display(void)
+{
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    // Change the view according to the position of the 'camera'
-    gluLookAt(0, 0.0, 7,
+    gluLookAt(realX, 0, realZ,
               0.0, 0.0, 0.0,
               0.0, 1.0, 0.0);
 
     drawRoom();
     drawRobot();
 
+    if (camY > 0 || jumpSpeed > 0)
+    {
+        camY += jumpSpeed;
+        jumpSpeed -= 0.002;
+        if (camY < 0)
+        {
+            camY = 0;
+            jumpSpeed = 0;
+        }
+    }
+
     glutSwapBuffers();
 }
 
-void reshape(int w, int h) {
+void reshape(int w, int h)
+{
     glViewport(0, 0, (GLsizei)w, (GLsizei)h);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluPerspective(60, (GLfloat)w / (GLfloat)h, 1.0, 100.0);
+    gluPerspective(90, (GLfloat)w / (GLfloat)h, 1.0, 100.0);
     glMatrixMode(GL_MODELVIEW);
 }
 
@@ -166,16 +178,34 @@ void handleKeypress(unsigned char key, int x, int y)
     switch (key)
     {
     case 'w':
-        camZ += 0.1;
+        if (camZ + 0.1 < 9.5)
+            camZ += 0.1;
         break;
     case 's':
-        camZ -= 0.1;
+        if (camZ - 0.1 > -9.5)
+            camZ -= 0.1;
         break;
     case 'a':
-        camX += 0.1;
+        if (camX + 0.1 < 9.5)
+            camX += 0.1;
+
         break;
     case 'd':
-        camX -= 0.1;
+        if (camX - 0.1 > -9.5)
+            camX -= 0.1;
+        break;
+    case 'm':
+        realX -= 0.1;
+        break;
+    case 'n':
+        realX += 0.1;
+        break;
+    case 'l':
+        realZ *= -1;
+        break;
+    case ' ':
+        if (camY == 0)
+            jumpSpeed = 0.06;
         break;
     }
 }
@@ -192,7 +222,7 @@ int main(int argc, char **argv)
     glutIdleFunc(display);
     glutReshapeFunc(reshape);
     glutKeyboardFunc(handleKeypress);
-    glClearColor(1,1,0,1);
+    glClearColor(1, 1, 0, 1);
     glutMainLoop();
     return 0;
 }
